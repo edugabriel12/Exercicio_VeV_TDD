032a463f34a5a950b88d5a70abb5b4cede8f9af9
edugabriel12 <gabriellima4539@yahoo.com>
2024-03-05 17:02:22 -0300

    Criando novo método de teste seguindo TDD deveCriarListaDePagamentos

--- ProcessadorBoletos/src/test/java/com/br/processadorboletos/service/ProcessadorBoletosServiceTest.java
+++ ProcessadorBoletos/src/test/java/com/br/processadorboletos/service/ProcessadorBoletosServiceTest.java
@@ -32,4 +32,35 @@ public class ProcessadorBoletosServiceTest {
 
         Assertions.assertTrue(fatura.getFaturaEstaPaga());
     }
+
+    @Test
+    public void deveCriarListaDePagamentos() {
+
+        Boleto boleto1 = new Boleto("1", new Date(), 450);
+        Boleto boleto2 = new Boleto("2", new Date(), 300);
+        Boleto boleto3 = new Boleto("3", new Date(), 400);
+
+        List<Boleto> boletos = Arrays.asList(boleto1, boleto2, boleto3);
+
+        Fatura fatura = new Fatura(
+                new Date(),
+                1000,
+                "Eduardo Gabriel",
+                false);
+
+        ProcessadorBoletosService service = new ProcessadorBoletosService();
+
+        List<Pagamento> pagamentos = service.processaListaBoletos(boletos, fatura);
+
+        Assertions.assertEquals(3, pagamentos.size());
+
+        Assertions.assertEquals(TipoPagamento.BOLETO, pagamentos.get(0).getTipoPagamento());
+        Assertions.assertEquals(450, pagamentos.get(0).getValorPago());
+
+        Assertions.assertEquals(TipoPagamento.BOLETO, pagamentos.get(1).getTipoPagamento());
+        Assertions.assertEquals(300, pagamentos.get(1).getValorPago());
+
+        Assertions.assertEquals(TipoPagamento.BOLETO, pagamentos.get(2).getTipoPagamento());
+        Assertions.assertEquals(400, pagamentos.get(2).getValorPago());
+    }
 }
