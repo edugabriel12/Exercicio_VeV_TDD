6197c6c5ae2d3c81180f1e283cb97078d2b707d8
edugabriel12 <gabriellima4539@yahoo.com>
2024-03-05 10:47:34 -0300

    Criando repositório para a especificação do Processador de Boletos

---
+++ ProcessadorBoletos/src/main/java/com/br/processadorboletos/ProcessadorBoletosApplication.java
@@ -0,0 +1,13 @@
+package com.br.processadorboletos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessadorBoletosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessadorBoletosApplication.class, args);
+    }
+
+}
---
+++ ProcessadorBoletos/src/test/java/com/br/processadorboletos/ProcessadorBoletosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.processadorboletos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessadorBoletosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
