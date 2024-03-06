4b7a18512429bca1e969dccca08f10fbc99deec4
francisco <lucas.araripina@hotmail.com>
2024-03-05 22:02:50 -0300

    criando funcao de ordenacao da lista

--- GerenciadorDeTarefas/src/main/java/com/br/gerenciadordetarefas/service/GerenciadorTarefasService.java
+++ GerenciadorDeTarefas/src/main/java/com/br/gerenciadordetarefas/service/GerenciadorTarefasService.java
@@ -1,8 +1,11 @@
 package com.br.gerenciadordetarefas.service;
 
+import com.br.gerenciadordetarefas.domain.Prioridade;
 import com.br.gerenciadordetarefas.domain.Tarefa;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class GerenciadorTarefasService {
@@ -20,11 +23,37 @@ public class GerenciadorTarefasService {
 
     // Método para excluir uma tarefa da lista
     public void excluirTarefa(Tarefa tarefa) {
+
         listaDeTarefas.remove(tarefa);
     }
 
     // Método para obter a lista de tarefas
     public List<Tarefa> getListaDeTarefas() {
+        ordenarListaDeTarefas();
         return listaDeTarefas;
+    }
+
+    private void ordenarListaDeTarefas() {
+        Collections.sort(listaDeTarefas, new Comparator<Tarefa>() {
+            @Override
+            public int compare(Tarefa t1, Tarefa t2) {
+                // Se a prioridade for alta, a tarefa deve vir primeiro
+                if (t1.getPrioridade() == Prioridade.ALTA && t2.getPrioridade() != Prioridade.ALTA) {
+                    return -1;
+                } else if (t1.getPrioridade() != Prioridade.ALTA && t2.getPrioridade() == Prioridade.ALTA) {
+                    return 1;
+                }
+
+                // Se a prioridade for média e a outra tarefa for de prioridade baixa, a tarefa deve vir primeiro
+                if (t1.getPrioridade() == Prioridade.MEDIA && t2.getPrioridade() == Prioridade.BAIXA) {
+                    return -1;
+                } else if (t1.getPrioridade() == Prioridade.BAIXA && t2.getPrioridade() == Prioridade.MEDIA) {
+                    return 1;
+                }
+
+                // Se as prioridades forem iguais, ordena por data
+                return t1.getData().compareTo(t2.getData());
+            }
+        });
     }
 }
--- GerenciadorDeTarefas/src/test/java/com/br/gerenciadordetarefas/service/GerenciadorTarefasTest.java
+++ GerenciadorDeTarefas/src/test/java/com/br/gerenciadordetarefas/service/GerenciadorTarefasTest.java
@@ -58,14 +58,16 @@ public class GerenciadorTarefasTest {
     public void deveRetornarOrdenado(){
         Tarefa tarefa3 = new Tarefa("Tarefa 3", "Descrição da tarefa 3",  new Date(2024, 3, 4), Prioridade.BAIXA);
         Tarefa tarefa1 = new Tarefa("Tarefa 1", "Descrição da tarefa 1", new Date(2024, 3, 5), Prioridade.ALTA);
+        Tarefa tarefa4 = new Tarefa("Tarefa 1", "Descrição da tarefa 1", new Date(2025, 3, 5), Prioridade.ALTA);
         Tarefa tarefa2 = new Tarefa("Tarefa 2", "Descrição da tarefa 2",  new Date(2024, 3, 6), Prioridade.MEDIA);
 
-        List<Tarefa> tarefas = Arrays.asList(tarefa3, tarefa2, tarefa1);
+        List<Tarefa> tarefas = Arrays.asList(tarefa3, tarefa2, tarefa1,tarefa4);
         GerenciadorTarefasService tarefaService = new GerenciadorTarefasService(tarefas);
         List<Tarefa> listaOrdenada = tarefaService.getListaDeTarefas();
         assertEquals(tarefa1, listaOrdenada.get(0));
-        assertEquals(tarefa2, listaOrdenada.get(1));
-        assertEquals(tarefa3, listaOrdenada.get(2));
+        assertEquals(tarefa4, listaOrdenada.get(1));
+        assertEquals(tarefa2, listaOrdenada.get(2));
+        assertEquals(tarefa3, listaOrdenada.get(3));
 
 
     }
